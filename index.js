7dccd13db804f943f33e03370aeb507420ac73c5
Jhon Jairo zabala <jhonnyzb1@hotmail.com>
2022-11-20 19:33:04 -0500

    ajuste download file verce2

--- index.js
+++ index.js
@@ -4,24 +4,20 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:4200'
+    origin: 'http://localhost:4200/'
 }));
 
 app.get("/", (req, res) => {
     return res.send('aqui')
 })
 
+app.get("/download1", (req, res) => {
+    return res.download("fondo1.png");
+    return res.send('aqui2')
+})
 
 app.get("/download/:guid", (req, res) => {
-    let min = 1;
-    let max = 2;
-    let x = Math.floor(Math.random() * (max - min + 1) + min);
-    let file = '';
-    if (x = 1) {
-        file = `${__dirname}/files/20MB.zip`;
-    }else{
-        file = `${__dirname}/files/10MB.zip`;
-    }
+    const file = `${__dirname}/files/10MB.zip`;
     return res.download(file);
 })
 
